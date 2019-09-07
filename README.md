0b8bad779b70545e3e50eb4c024413fd098286ee
Sysca11 <3321056730@qq.com>
2019-09-08 00:17:20 +0800

    upload

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Optimize Minecraft Bedrock Server by hot function rewriting and auto inlining
 <pre>
 Complie Rocket:
 g++ -Ofast -march=native rock.cpp LDasm.c -o rocket
-g++ -Ofast -march=native optim.cpp -o optiserver
+gcc -Ofast -march=native optim.cpp -o optiserver
 Run Rocket:
 ./rocket
 Run Server:
---
+++ optim.c
@@ -0,0 +1,104 @@
+typedef unsigned long u64;
+typedef signed long s64;
+typedef unsigned int u32;
+u64 P_ZNKSt8__detail10_Hash_nodeISt4pairIK8ChunkPosSt8weak_ptrI10LevelChunkEELb1EE7_M_nextEv(u64* a){
+return *a;
+}
+u64 P_ZSt4moveIRKSaI24ApplyLegacyEntityBindingEEONSt16remove_referenceIT_E4typeEOS5_(u64 a){
+return a;
+}
+void P_ZN19BlockSourceListenerD2Ev(){
+}
+u64 P_ZNSt14_Sp_ebo_helperILi0ESaI25ScriptSpawnActorComponentELb1EE6_S_getERS2_(u64 a){
+return a;
+}
+void P_ZNSt14default_deleteI18OceanMonumentPieceEC2I24OceanMonumentDoubleZRoomvEERKS_IT_E(){
+}
+u64 P_ZNKSt12__shared_ptrIN8JsonUtil14JsonSchemaNodeINS0_14JsonParseStateINS0_10EmptyClassE19ActorAnimationGroupEE10SemVersionEELN9__gnu_cxx12_Lock_policyE2EE3getEv(u64* a){
+return *a;
+}
+void P_ZNSt8_Rb_treeI10BlockShapeSt4pairIKS0_St6vectorINSt7__cxx1112basic_stringIcSt11char_traitsIcESaIcEEESaIS9_EEESt10_Select1stISC_ESt4lessIS0_ESaISC_EE11_Alloc_nodeC2ERSI_(u64* a,u64 b){
+*a=b;
+}
+void P_ZN9__gnu_cxx17__normal_iteratorIPN12GoalSelector12InternalGoalESt6vectorIS2_SaIS2_EEEC2ERKS3_(u64* a,u64* b){
+*a=*b;
+}
+void P_ZNSt10_Head_baseILm0EPN9ScriptApi20EmptyScriptInterfaceELb0EEC2Ev(u64* a){
+*a=0;
+}
+u64 P_ZNKSt10_HashtableI12HashedStringSt4pairIKS0_St10shared_ptrIN8JsonUtil25JsonSchemaChildOptionBaseINS4_14JsonParseStateINS4_10EmptyClassENS4_7details14BlockReferenceEEE11CompoundTagEEEESaISE_ENSt8__detail10_Select1stESt8equal_toIS0_ESt4hashIS0_ENSG_18_Mod_range_hashingENSG_20_Default_ranged_hashENSG_20_Prime_rehash_policyENSG_17_Hashtable_traitsILb1ELb0ELb1EEEE8_M_beginEv(u64* a){
+	return *(a+2);
+}
+u64* P_ZNSt15__allocated_ptrISaISt23_Sp_counted_ptr_inplaceIN8JsonUtil21JsonSchemaChildOptionINS1_14JsonParseStateINS3_INS3_INS3_INS3_INS1_10EmptyClassE19ActorAnimationGroupEES5_EE22ActorSkeletalAnimationEES8_EE13BoneAnimationEENS3_ISC_17KeyFrameTransformEE14ExpressionNodeEESaISG_ELN9__gnu_cxx12_Lock_policyE2EEEEaSEDn(u64* a){
+*(a+1)=0;
+return a;
+}
+u64 P_ZNK7WeakPtrI11BlockLegacyEdeEv(u64** a){
+return **a;
+}
+int* P_ZSt3maxIiERKT_S2_S2_(int* a,int* b){
+	return *a>*b?a:b;
+}
+u64 P_ZNK11BlockLegacyeqERKS_(u64 a,u64 b){
+	return a==b;
+}
+u64 P_ZNK8ChunkPoseqERKS_(u64* a,u64* b){
+	return *a==*b;
+}
+u64 P_ZNK11BlockLegacyneERKS_(u64 a,u64 b){
+	return a!=b;
+}
+char P_ZN7NewTypeIhEC2ERKh(char* a1,char* a2){
+	return *a1=*a2;
+}
+char P_ZN7NewTypeIhEC2ERKS0_(char* a1,char* a2){
+	return *a1=*a2;
+}
+u64 P_ZNKSt10unique_ptrI25SubChunkBrightnessStorageSt14default_deleteIS0_EE3getEv(u64* a){
+return *a;
+}
+typedef struct Vec3{
+	float x,y,z;
+} Vec3;
+float P_ZNK4Vec33dotERKS_(Vec3* a,Vec3* b){
+	return a->x*b->x+a->y*b->y+a->z*b->z;
+}
+Vec3* P_ZN4Vec3C2Efff(Vec3* a,float b,float c,float d){
+*a=(Vec3){b,c,d};
+return a;
+}
+u64 P_ZNK5Block14getLegacyBlockEv(u64** a){
+	return **(a+2);
+}
+
+char P_ZNKSt6atomicI10ChunkStateEcvS0_Ev(char* a){
+return *a;
+}
+char P_ZNKSt6atomicI10ChunkStateE4loadESt12memory_order(char* a){
+return *a;
+}
+
+void P_ZNSt6atomicI10ChunkStateE5storeES0_St12memory_order(char* a,char b){
+	*a=b;
+}
+
+
+u64 P_ZNK8SubChunk8getBlockEt(u64 th,unsigned short a2){
+return (*(u64 (**)(u64, u64))(**((u64 **)th + 4) + 24LL))(*((u64 *)th + 4), a2);
+}
+
+u64 P_ZNKSt6vectorI8EntityIdSaIS0_EE4sizeEv(u64* a1){
+return (a1[1] - *a1) >> 2;
+}
+u64 P_ZNKSt6atomicIP20SubChunkBlockStorageE4loadESt12memory_order(u64* a){
+return *a;
+}
+u64 P_ZNK5RangeIiLi1EE8iteratorneERKS1_(u64* a,u64* b){
+	return *a!=*b;
+}
+//patch for _ZNKSt10unique_ptrI11ChunkSourceSt14default_deleteIS0_EEptEv  St10unique_ptr EEptEv EEdeEv EE3getEv
+//_ZNKSt19__shared_ptr_accessI29ResourcePackFileUploadManagerLN9__gnu_cxx12_Lock_policyE2ELb0ELb0EEptEv   St19__shared_ptr_access  EEptEv EEdeEv
+
+int main(){
+
+}
